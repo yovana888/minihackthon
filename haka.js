b445504f660a7b7ed778f26b41a1ece3d4620875
JesusRuizWong <jesusruizwong@outlook.com>
2022-11-05 14:39:23 -0500

    bisiestos

---
+++ haka.js
@@ -0,0 +1,25 @@
+//Programa que calcula si un año es bisiesto 
+const calcular=document.getElementById("calcular") ;
+const year=document.getElementById("año") ;
+
+
+calcular.onclick = function() {       
+ if(year.value ===""){
+         Swal.fire({
+             title: "Ayuda",
+             text: "Indicanos un año primero",
+             icon:"alert"        
+         });
+         return;
+}
+
+
+ if(((year.value%4==0)&&(year.value%100!=0))||year.value%400==0)
+ { document.getElementById('mensaje').value= "'"+ `${year.value}` +"'" + " ES UN AÑO BISIESTO"  ;
+   document.getElementById('año').value= "" ;
+  }           
+ else    { document.getElementById('mensaje').value= "'"+ `${year.value}` +"'"  + " NO ES UN AÑO BISIESTO"; 
+ document.getElementById('año').value= "" ;
+
+
+} }
